b2e4cc4bcffee19d335dafccf72c02851174f838
MathiasFM <mathiasmogensen1@gmail.com>
2022-07-10 00:30:08 +0200

    added gamescore, but bugs with keeping score

--- javascript_rock_paper_scissor/rock_paper_scissor.js
+++ javascript_rock_paper_scissor/rock_paper_scissor.js
@@ -1,7 +1,9 @@
 function randomNum() {
     return Math.floor(Math.random() * 100) + 1;
 }
-
+let playerScore = 0;
+let computerScore = 0;
+let tie = 0;
 function computerPlay() {
     //RPS = rock, paper, scissor
     let computerRPS = randomNum();
@@ -22,7 +24,7 @@ function playRound(playerSelection, computerSelection) {
     let playerSelectionCaseIn = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
     if(computerSelection == playerSelectionCaseIn) {
-        return `It's a Tie! Computer chose ${computerSelection}, and you also chose ${playerSelectionCaseIn}. Try again!`;
+        return tieRound, `It's a Tie! Computer chose ${computerSelection}, and you also chose ${playerSelectionCaseIn}. Try again!`;
     } else if(computerSelection == "Rock" && playerSelectionCaseIn == "Scissor") {
         return `You Lose! ${computerSelection} beats ${playerSelectionCaseIn}`;
     } else if(computerSelection == "Paper" && playerSelectionCaseIn == "Rock") {
@@ -30,24 +32,40 @@ function playRound(playerSelection, computerSelection) {
     } else if(computerSelection == "Scissor" && playerSelectionCaseIn == "Paper") {
         return `You Lose! ${computerSelection} beats ${playerSelectionCaseIn}`;
     } else if(playerSelectionCaseIn != "Rock" && playerSelectionCaseIn != "Paper" && playerSelectionCaseIn != "Scissor") {
-        return `${playerSelectionCaseIn} is not accepted, please try again.`
+        return tieRound, `${playerSelectionCaseIn} is not accepted, please try again.`
     } else {
-        return `You Win! ${playerSelectionCaseIn} beats ${computerSelection}`;
+        return playerWinRound, `You Win! ${playerSelectionCaseIn} beats ${computerSelection}`;
     }
 }
 
-const playerSelection = "PapEr";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+let playerWinRound = "You win"
+let tieRound = "tie"
+//AND method: else if(playerSelectionCaseIn != "Rock" && playerSelectionCaseIn != "Paper" && playerSelectionCaseIn != "Scissor") 
+//OR method: else if(!(playerSelectionCaseIn == "Rock" || playerSelectionCaseIn == "Paper" || playerSelectionCaseIn == "Scissor"))
+//, `You Win! ${playerSelectionCaseIn} beats ${computerSelection}`
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        console.log(playRound( playerSelection, computerSelection));
+        const playerSelection = "rock";
+        const computerSelection = computerPlay();
+        let result = playRound(playerSelection, computerSelection);
+        console.log(gameScore(result))
+        console.log(tie);
+        console.log("Your score is " + playerScore);
+        console.log("The computers score is " + computerScore);
+        console.log(playRound(playerSelection, computerSelection));
+        
+        
     }
 }
+
 console.log(game())
-
-//AND method: else if(playerSelectionCaseIn != "Rock" && playerSelectionCaseIn != "Paper" && playerSelectionCaseIn != "Scissor") 
-//OR method: else if(!(playerSelectionCaseIn == "Rock" || playerSelectionCaseIn == "Paper" || playerSelectionCaseIn == "Scissor"))
-
-
+function gameScore(result) {
+    if(result == playerWinRound) {
+        playerScore++;
+    } else if(result == tieRound) {
+        tie;
+    } else {
+        computerScore++;
+    }
+}
