e9fe986e831771f48cd7d674d6aad35fdf1d1dd2
MathiasFM <mathiasmogensen1@gmail.com>
2022-07-11 02:06:30 +0200

    Fixed score system, added prompt for playerinput

--- javascript_rock_paper_scissor/rock_paper_scissor.js
+++ javascript_rock_paper_scissor/rock_paper_scissor.js
@@ -1,9 +1,7 @@
 function randomNum() {
-    return Math.floor(Math.random() * 100) + 1;
+    return Math.floor(Math.random() * 100);
 }
-let playerScore = 0;
-let computerScore = 0;
-let tie = 0;
+
 function computerPlay() {
     //RPS = rock, paper, scissor
     let computerRPS = randomNum();
@@ -18,54 +16,57 @@ function computerPlay() {
     
 }
 
+let playerScore = 0;
+let computerScore = 0;
+let gameError = 0;
+
 //R>S S>P P>R
 function playRound(playerSelection, computerSelection) {
 
     let playerSelectionCaseIn = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
     if(computerSelection == playerSelectionCaseIn) {
-        return tieRound, `It's a Tie! Computer chose ${computerSelection}, and you also chose ${playerSelectionCaseIn}. Try again!`;
+        return `It's a Tie! Computer chose ${computerSelection}, and you also chose ${playerSelectionCaseIn}. Try again!`;
     } else if(computerSelection == "Rock" && playerSelectionCaseIn == "Scissor") {
+        computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelectionCaseIn}`;
     } else if(computerSelection == "Paper" && playerSelectionCaseIn == "Rock") {
+        computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelectionCaseIn}`;
     } else if(computerSelection == "Scissor" && playerSelectionCaseIn == "Paper") {
+        computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelectionCaseIn}`;
     } else if(playerSelectionCaseIn != "Rock" && playerSelectionCaseIn != "Paper" && playerSelectionCaseIn != "Scissor") {
-        return tieRound, `${playerSelectionCaseIn} is not accepted, please try again.`
+        gameError++;
+        return `${playerSelectionCaseIn} is not accepted, please try again.`
     } else {
-        return playerWinRound, `You Win! ${playerSelectionCaseIn} beats ${computerSelection}`;
+        playerScore++;
+        return `You Win! ${playerSelectionCaseIn} beats ${computerSelection}`;
     }
 }
 
-let playerWinRound = "You win"
-let tieRound = "tie"
+let playerWinRound = ""
+let tieRound = ""
 //AND method: else if(playerSelectionCaseIn != "Rock" && playerSelectionCaseIn != "Paper" && playerSelectionCaseIn != "Scissor") 
 //OR method: else if(!(playerSelectionCaseIn == "Rock" || playerSelectionCaseIn == "Paper" || playerSelectionCaseIn == "Scissor"))
 //, `You Win! ${playerSelectionCaseIn} beats ${computerSelection}`
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = "rock";
+    for (let i = 0; i < 50; i++) {
+        const playerSelection = prompt('Choose "Rock", "Paper" or "Scissor"');
         const computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(gameScore(result))
-        console.log(tie);
-        console.log("Your score is " + playerScore);
-        console.log("The computers score is " + computerScore);
         console.log(playRound(playerSelection, computerSelection));
         
+        if(playerScore == 5 || computerScore == 5) {
+            break;
+        } else if(gameError > 0) {
+            break;
+        }
+        
+        
         
     }
+    console.log("Your score is " + playerScore);
+    console.log("The computers score is " + computerScore);
 }
-
-console.log(game())
-function gameScore(result) {
-    if(result == playerWinRound) {
-        playerScore++;
-    } else if(result == tieRound) {
-        tie;
-    } else {
-        computerScore++;
-    }
-}
+game()
